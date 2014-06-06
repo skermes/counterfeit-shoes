844cf41f0730d9ea6296299136c236be58b74021
Sean Kermes <skermes@gmail.com>
2014-06-06 17:17:22 -0400

    App component listens to router store.

--- src/shared/app.js
+++ src/shared/app.js
@@ -3,9 +3,22 @@
 var React = require('react');
 
 var Toolbar = require('../toolbar/toolbar'),
-    DeckViewer = require('../deck/deck-viewer');
+    DeckViewer = require('../deck/deck-viewer'),
+    RouterStore = require('../stores/router-store');
 
 var App = React.createClass({
+  getInitialState: function() {
+    return {
+      pathSegments: RouterStore.pathSegments()
+    };
+  },
+  componentDidMount: function() {
+    RouterStore.listen(this._onPathChange);
+  },
+  componentWillUnmount: function() {
+    RouterStore.ignore(this._onPathChange);
+  },
+
   render: function() {
     return (
       <div className="container">
@@ -13,6 +26,12 @@ var App = React.createClass({
         <DeckViewer />
       </div>
     );
+  },
+
+  _onPathChange: function() {
+    this.setState({
+      pathSegments: RouterStore.pathSegments()
+    });
   }
 });
 
