6af09b8b1ac6d8a5a3bbad93bd1ebfc9c59e217f
Sean Kermes <skermes@gmail.com>
2014-06-06 15:17:35 -0400

    Remove test deck from deck store.

--- src/stores/deck-store.js
+++ src/stores/deck-store.js
@@ -5,29 +5,7 @@ var Store = require('./store'),
 
 var DeckStore = new Store({
   initialize: function() {
-    // this.deck = {};
-
-    this.deck = {
-      'sparkbot': {quantity: 3, card: Card('spark bot')},
-      'ionicwarcharger': {quantity: 3, card: Card('ionic warcharger')},
-      'heavyartillery': {quantity: 2, card: Card('heavy artillery')},
-      'cypienbattlesuit': {quantity: 2, card: Card('cypien battlesuit')},
-      'oreianbattledroid': {quantity: 2, card: Card('oreian battledroid')},
-      'aetherguard': {quantity: 2, card: Card('aetherguard')},
-      'matrixwarden': {quantity: 2, card: Card('matrix warden')},
-      'nexuspilot': {quantity: 2, card: Card('nexus pilot')},
-      'techupgrade': {quantity: 2, card: Card('tech upgrade')},
-      'jetpack': {quantity: 1, card: Card('jet pack')},
-      'digitize': {quantity: 1, card: Card('digitize')},
-      'synapsisoracle': {quantity: 1, card: Card('synapsis oracle')},
-      'munitionsdrone': {quantity: 1, card: Card('munitions drone')},
-      'vaultblockade': {quantity: 1, card: Card('vault blockade')},
-      'battletechtician': {quantity: 1, card: Card('battle techtician')},
-      'forgeplatesentry': {quantity: 1, card: Card('forgeplate sentry')},
-      'oreianjusticar': {quantity: 1, card: Card('oreian justicar')},
-      'arcflightsquadron': {quantity: 1, card: Card('arcflight squadron')},
-      'forgeguardianbeta': {quantity: 1, card: Card('forge guardian beta')},
-    };
+    this.deck = {};
   },
 
   dispatches: {
