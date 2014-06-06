08d61c2b7bd5acfa6b7aab9db1abc9fddb02313b
Sean Kermes <skermes@gmail.com>
2014-06-06 16:31:28 -0400

    Enable the React dev tools.

--- src/counterfeit-shoes.js
+++ src/counterfeit-shoes.js
@@ -8,3 +8,6 @@ document.addEventListener('DOMContentLoaded', function() {
   React.renderComponent(<App />,
                         document.getElementById('root'))
 });
+
+// Enables the React dev tools.
+window.React = React;
