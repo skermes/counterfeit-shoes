f84759549e8e3a6d6a672b9531ada03253d97160
Sean Kermes <skermes@gmail.com>
2014-06-06 18:31:21 -0400

    RouterStore sends action on url change.

--- src/stores/router-store.js
+++ src/stores/router-store.js
@@ -1,10 +1,16 @@
-var Store = require('./store');
+var Store = require('./store'),
+    Dispatcher = require('../dispatcher');
 
 var RouterStore = new Store({
   initialize: function() {
     window.onpopstate = function() {
+      Dispatcher.send('pathChanged', [this.pathSegments]);
       this.trigger();
     }.bind(this);
+
+    // Some browsers fire a popstate on page load, some don't.  Store functions
+    // should be idempotent, so sending this twice shouldn't be a problem.
+    Dispatcher.send('pathChanged', [this.pathSegments]);
   },
 
   pathSegments: function() {
