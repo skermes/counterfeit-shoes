63cff33c8e6b679ab1029dcde54ccf061eaac675
Sean Kermes <skermes@gmail.com>
2014-06-06 17:47:10 -0400

    Delete logo component.
    Wasn't really working out.

--- src/toolbar/toolbar.js
+++ src/toolbar/toolbar.js
@@ -3,8 +3,7 @@
 var React = require('react');
 
 var CardSearch = require('../search/card-search'),
-    SaveButton = require('./save-button'),
-    Logo = require('./logo');
+    SaveButton = require('./save-button');
 
 var Toolbar = React.createClass({
   render: function() {
